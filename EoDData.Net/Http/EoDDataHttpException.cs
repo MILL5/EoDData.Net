8f9fa5d0df44bd0663d8a96db32e6b4d7bda2474
Esteban Lalinde <84730201+estebanl5@users.noreply.github.com>
2021-06-10 16:53:40 -0500

    technical debt and coverage results

--- EoDData.Net/Http/EoDDataHttpException.cs
+++ EoDData.Net/Http/EoDDataHttpException.cs
@@ -14,11 +14,6 @@ namespace EoDData.Net
         protected EoDDataHttpException(SerializationInfo info, StreamingContext context)
         : base(info, context)
         {
-        }
-
-        public override void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            base.GetObjectData(info, context);
-        }
+        }        
     }
 }
