2b75535e90dc1d2ff6ac8a8e130af42af95ab48a
Nathan Franz <nathanf@mill5.com>
2022-02-21 11:50:27 -0500

    hardcode base url

--- EoDData.Net/EoDDataSettings.cs
+++ EoDData.Net/EoDDataSettings.cs
@@ -4,6 +4,9 @@
     {
         private const string EODDATA_HTTPCLIENT_NAME = "EoDDataHttpClient";
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Minor Code Smell", "S1075:URIs should not be hardcoded", Justification = "Need to use const to build Urls")]
+        private const string EODDATA_BASE_Url = "http://ws.eoddata.com/data.asmx/";
+
         public string ApiUsername { get; }
 
         public string ApiPassword { get; }
@@ -16,9 +19,9 @@
 
         internal string HttpClientName { get; } = EODDATA_HTTPCLIENT_NAME;
 
-        public EoDDataSettings(string apiBaseUrl, string apiUsername, string apiPassword)
+        public EoDDataSettings(string apiUsername, string apiPassword)
         {
-            ApiBaseUrl = apiBaseUrl;
+            ApiBaseUrl = EODDATA_BASE_Url;
             ApiUsername = apiUsername;
             ApiPassword = apiPassword;
         }
